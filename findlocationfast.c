54de023b1be79fea5c7e8096d3f4f01d6996df33
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-09-10 22:03:13 -0600

    Last part

---
+++ findlocationfast.c
@@ -0,0 +1,11 @@
+#include <fcntl.h>  //open(), close()
+#include <unistd.h> //read(), ssize_t
+#include <stdio.h>  //fprintf(), stderr
+#include <errno.h>  //errno
+#include <string.h> //strerror()
+#include "my_c.h"
+
+int main(int argc, char **argv)
+{
+  return 0;
+}
