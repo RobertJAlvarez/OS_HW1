6a631bba8501e584a48a8e91374a124a0e8f9ebb
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-09-10 21:09:46 -0600

    Get to know the bash shell is done, except for the findlocation

--- intro_bash.sh
+++ intro_bash.sh
@@ -10,3 +10,12 @@ cat /proc/cpuinfo | grep -i "Model" | sed -n '1p'
 echo ---------------------------
 cat /proc/meminfo | grep -i "MemTotal"
 echo -----------------------------
+
+temp=$( cat nanpa | wc -l)
+echo nanpa file contains $temp lines.
+
+echo The first 17 lines of nanpa are:
+head -n 17 nanpa
+echo The last 42 lines of nanpa are:
+tail -n 42 nanpa
+
