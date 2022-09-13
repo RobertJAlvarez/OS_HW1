ab9123285c5402df4c2d2560d1ba2dd5317b8e24
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-09-12 21:17:30 -0600

    Some commands added for testing edge case

--- intro_bash.sh
+++ intro_bash.sh
@@ -19,3 +19,7 @@ head -n 17 nanpa
 echo The last 42 lines of nanpa are:
 tail -n 42 nanpa
 
+##This next part is to test something, plis don't erase it
+#dd if=/dev/zero bs=512 count=1000000 | tr '\0' 'A' > file1
+#./head -n 5 < file1 > file2
+#diff file1 file2
